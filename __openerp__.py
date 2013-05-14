42a976ec58c097b5956d1934f11a167bb54f43b7
davidbeal <david.beal@akretion.com>
2013-05-14 11:43:58 +0200

    [INIT] add a button to directly launch the job

---
+++ __openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Sébastien BEAU
+#    Copyright 2013 Akretion SARL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Connector dev',
+ 'version': '7.0.1',
+ 'author': 'Openerp Connector Core Editors',
+ 'website': 'https://launchpad.net/openerp-connector',
+ 'license': 'AGPL-3',
+ 'category': 'Generic Modules',
+ 'description': """
+Helper to connector to retry instantly to perform a job.
+It add a button in backend to retry failed job.
+""",
+ 'depends': ['connector'
+             ],
+ 'data': ['job_view.xml',
+          ],
+ 'installable': True,
+ 'application': False,
+}
---
+++ queue.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Sebastien BEAU
+#    Copyright 2013 Akretion SARL
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+
+from openerp.osv import orm, fields
+from openerp.addons.connector.queue.job import OpenERPJobStorage
+from openerp.addons.connector.session import ConnectorSession
+
+
+class QueueJob(orm.Model):
+    _inherit = 'queue.job'
+
+    def run_now(self, cr, uid, ids, context=None):
+        session = ConnectorSession(cr, uid, context=context)
+        storage = OpenERPJobStorage(session)
+        for job in self.browse(cr, uid, ids, context=context):
+            job = storage.load(job.uuid)
+            job.perform(session)
+        return True
