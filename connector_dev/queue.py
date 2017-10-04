fcf994def3b85f7b669e6ef11eb043a649df87b4
Sébastien BEAU <sebastien.beau@akretion.com>
2017-10-04 10:41:41 +0200

    [MIG] migrate to version 10 connector_dev

--- connector_dev/queue.py
+++ connector_dev/queue.py
@@ -1,43 +1,17 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Sebastien BEAU
-#    Copyright 2013 Akretion SARL
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-#############################################################################
+# Copyright 2013 Akretion (http://www.akretion.com).
+# @author Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp.osv import orm
-from openerp.addons.connector.queue.job import OpenERPJobStorage
-from openerp.addons.connector.session import ConnectorSession
+from openerp.addons.queue_job.job import Job
+from openerp import api, fields, models
 
 
-class QueueJob(orm.Model):
+class QueueJob(models.Model):
     _inherit = 'queue.job'
-    
-    def run_now(self, cr, uid, ids, context=None):
-        session = ConnectorSession(cr, uid, context=context)
-        storage = OpenERPJobStorage(session)
-        for oe_job in self.browse(cr, uid, ids, context=context):
-            job = storage.load(oe_job.uuid)
-            job.perform(session)
-            oe_job.write({'state': 'done'})
-        return True
 
-    def run_queue_job_cron(self, cr, uid, ids, context=None):
-        cron_id = self.pool['ir.model.data'].get_object_reference(
-            cr, uid, 'connector', 'ir_cron_enqueue_jobs')[1]
-        self.pool['ir.cron'].run_manually(cr, uid, [cron_id], context=context)
+    def run_now(self):
+        for job in self:
+            Job.load(self.env, job.uuid).perform()
+            job.state = 'done'
         return True
